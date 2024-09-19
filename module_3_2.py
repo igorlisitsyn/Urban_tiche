ae467759e7ea1fff1dc840e708d9d422bc3c8df0
igor <bit1@list.ru>
2024-09-19 19:44:19 +1200

    Задача "Рассылка писем": исправлено с учетом замечаний

--- module_3_2.py
+++ module_3_2.py
@@ -26,22 +26,25 @@ def send_email(message, recipient, *, sender = "university.help@gmail.com"):
     matces = seach(recipient, sender)
     if recipient == sender:
         print(f'Нельзя отправить письмо самому себе!')
-        exit()
-    if sender == SENDER_:
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-        exit()
 
-    if matches_domen and matces and sender != SENDER_:
+    elif sender == SENDER_:
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+
+
+    elif matches_domen and matces and sender != SENDER_:
 
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-        exit()
+
     else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-        exit()
+
 
 
 
 def main():
+    send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+    send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+    send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
     send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 
 
