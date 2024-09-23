980f52c620429570b50a0bb97f671581eca46d58
igor <bit1@list.ru>
2024-09-23 20:10:44 +1200

    Задача "Рекурсивное умножение цифр":_ вариант с int

--- module_3_5.py
+++ module_3_5.py
@@ -1,19 +1,19 @@
 
 
 def get_multiplied_digits(num):
-
-    if "0" in num:
-        num = num.replace("0", "")
-    first = int(num[0])
-    if len(num) == 1:
-        return first
-    return first * get_multiplied_digits(num[1:])
+   str_number = str(num)
+   if "0" in str_number:
+       str_number = str_number.replace("0", "")
+   first = int(str_number[0])
+   if len(str_number) == 1:
+       return first
+   return first * get_multiplied_digits(int(str_number[1:]))
 
 
 
 
 def main():
-   print(get_multiplied_digits('00123'))
+  print(get_multiplied_digits(40203))
 
 if __name__ == '__main__':
-    main()
+   main()
