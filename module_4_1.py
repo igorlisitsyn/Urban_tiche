b75971dc5b73fed5c9d2c250d1da62d0a9948d3a
igor <bit1@list.ru>
2024-09-30 21:18:06 +1200

    Задача "А как делить?":

---
+++ module_4_1.py
@@ -0,0 +1,21 @@
+
+from true_math import true_divide
+from fake_math import fake_divide
+
+
+
+
+def main():
+    result1 = fake_divide(69, 3)
+    result2 = fake_divide(3, 0)
+    result3 = true_divide(49, 7)
+    result4 = true_divide(15, 0)
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+
+def true_divide(first, second):
+    if second == 0:
+        return inf
+    return first / second
+
+
+
+
