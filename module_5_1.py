ad1e2a9c1e7f93c2115c04a60d780e7b411cf976
igor <bit1@list.ru>
2024-10-12 19:48:52 +1200

    Задача "Магические здания"

--- module_5_1.py
+++ module_5_1.py
@@ -11,14 +11,22 @@ class House:
             for floor in range(1, new_floor+1):
                 print(floor)
 
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __len__(self):
+        return self.number_of_floors
 
 def main():
     h1 = House('ЖК Горский', 18)
 
     h2 = House('Домик в деревне', 2)
 
-    h1.go_to(5)
-    h2.go_to(10)
+    print(h1)
+    print(h2)
+
+    print(len(h1))
+    print(len(h2))
 
 if __name__ == '__main__':
     main()
