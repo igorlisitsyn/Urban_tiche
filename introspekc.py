bcd1105b97cc587407e1d9b473fa53d12a3e821e
igor <bit1@list.ru>
2024-12-13 20:32:40 +1200

    Задача "интроспекция объекта"

--- introspekc.py
+++ introspekc.py
@@ -21,10 +21,14 @@ def main():
 
 
     met = [i[0] for  i in inspect.getmembers(Book, inspect.isfunction)]
+
     intro['type'] = type(pp)
+    intro['attributes'] = list(pp.__dict__.keys())
     intro['methods Class'] = met
+    intro['methods'] = [met for met in pp.__dir__() if met.startswith('__')]
     intro['описание'] = inspect.getdoc(pp)
     intro['имя исполняемого файла'] = inspect.getfile(Book)
+    intro['module'] = str(inspect.getmodule(Book)).split()[1]
     print(intro)
 
 
